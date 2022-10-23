faa01a06973865d380186a79a2942acb616b7043
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-23 14:36:39 +0200

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <input type="text" placeholder="Message"/>
+      <button>Send Message</button>
     </div>
   );
 }
