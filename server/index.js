d48a2097ea9853ccf0cc09d2333982a2a1195b6a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-23 14:24:34 +0200

    Backend setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const http = require("http");
-const {Server} = require("socket.io")
+const {Server} = require("socket.io");
+const cors = require("cors");
 
+app.use(cors())
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -10,4 +12,9 @@ const io = new Server(server, {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
+})
+
+
+server.listen(8000, () => {
+  console.log("Server is running")
 })
