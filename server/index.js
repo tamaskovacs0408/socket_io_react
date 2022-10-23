badd61d7a3dd76e8aca212c70c30f87f71adfbe9
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-23 16:01:31 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
   })
 
   socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data)
+    socket.to(data.room).emit("receive_message", data)
   })
 })
 
